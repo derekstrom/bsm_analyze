17c59430327dcaf31367851c8f0806ba76c9272a
Samvel Khalatyan <ksamdev@gmail.com>
2011-05-05 10:51:20 -0500

    Add clone() to Analyzer interface

--- interface/Analyzer.h
+++ interface/Analyzer.h
@@ -9,6 +9,8 @@
 #ifndef BSM_CORE_ANALYZER
 #define BSM_CORE_ANALYZER
 
+#include <boost/shared_ptr.hpp>
+
 #include <iosfwd>
 
 namespace bsm
@@ -20,14 +22,26 @@ namespace bsm
         class Analyzer
         {
             public:
+                typedef boost::shared_ptr<Analyzer> AnalyzerPtr;
+
                 Analyzer();
                 virtual ~Analyzer();
+
+                // Custom analyzers know how to clone themself
+                //
+                virtual AnalyzerPtr clone() const = 0;
 
                 virtual void process(const Event *) = 0;
 
                 // Each analyzer should know how to print itself on screen
                 //
                 virtual void print(std::ostream &) const = 0;
+
+            private:
+                // Prevent Copying: clone() should be used instead
+                //
+                Analyzer(const Analyzer &);
+                Analyzer &operator =(const Analyzer &);
         };
 
         // Helpers
