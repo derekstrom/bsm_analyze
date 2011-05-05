10f9fb752fab6eef3e9ee3b5591178dd04def22d
Samvel Khalatyan <ksamdev@gmail.com>
2011-05-05 16:44:34 -0500

    Use threads in the monitor input

--- src/monitor_input.cpp
+++ src/monitor_input.cpp
@@ -13,9 +13,11 @@
 #include <TH1.h>
 #include <TRint.h>
 
-#include "bsm_input/interface/Reader.h"
-#include "bsm_input/interface/Event.pb.h"
+#include "bsm_stat/interface/Utility.h"
+
 #include "interface/Monitor.h"
+#include "interface/MonitorAnalyzer.h"
+#include "interface/Thread.h"
 
 using std::cerr;
 using std::cout;
@@ -23,17 +25,18 @@ using std::endl;
 
 using boost::shared_ptr;
 
-using bsm::Reader;
-using bsm::Event;
-using bsm::ElectronMonitor;
-using bsm::GenParticleMonitor;
-using bsm::JetMonitor;
-using bsm::MissingEnergyMonitor;
-using bsm::MuonMonitor;
-using bsm::PrimaryVertexMonitor;
+using bsm::MonitorAnalyzer;
+using bsm::core::Files;
+using bsm::core::ThreadController;
+using bsm::stat::convert;
+using bsm::stat::TH1Ptr;
+
+typedef shared_ptr<MonitorAnalyzer> MonitorAnalyzerPtr;
+
+void run(const Files &);
+void plot(const MonitorAnalyzerPtr &);
 
 int main(int argc, char *argv[])
-try
 {
     if (2 > argc)
     {
@@ -44,155 +47,140 @@ try
 
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    shared_ptr<ElectronMonitor> electrons(new ElectronMonitor());
-    shared_ptr<GenParticleMonitor> gen_particles(new GenParticleMonitor());
-    shared_ptr<JetMonitor> jets(new JetMonitor());
-    shared_ptr<MissingEnergyMonitor> missing_energy(new MissingEnergyMonitor());
-    shared_ptr<MuonMonitor> muons(new MuonMonitor());
-    shared_ptr<PrimaryVertexMonitor> primary_vertices(new PrimaryVertexMonitor());
-
+    int result = 0;
+    try
     {
-        shared_ptr<Reader> reader(new Reader(argv[1]));
-        uint32_t events_read = 0;
-        for(shared_ptr<Event> event(new Event());
-                reader->read(*event);
-                ++events_read)
-        {
-            jets->fill(event->jets());
+        Files input_files;
+        for(int i = 2; argc > i; ++i)
+            input_files.push_back(argv[i]);
 
-            for(JetMonitor::Jets::const_iterator jet = event->jets().begin();
-                    event->jets().end() != jet;
-                    ++jet)
-            {
-                if (!jet->has_gen_parton())
-                    continue;
-
-                gen_particles->fill(jet->gen_parton());
-            }
-
-            muons->fill(event->muons());
-            electrons->fill(event->electrons());
-            primary_vertices->fill(event->primary_vertices());
-
-            if (event->has_missing_energy())
-                missing_energy->fill(event->missing_energy());
-
-            event->Clear();
-        }
-
-        cout << "Events Read: " << events_read << endl;
+        run(input_files);
     }
-
+    catch(...)
     {
-        /*
-        int empty_argc = 1;
-        char *empty_argv[] = { argv[0] };
-        shared_ptr<TRint> app(new TRint("app", &empty_argc, empty_argv));
+        cerr << "Unknown error" << endl;
 
-        shared_ptr<TCanvas> jet_canvas(new TCanvas("jets", "Jets", 800, 320));
-        jet_canvas->Divide(3);
-
-        jet_canvas->cd(1);
-        jets->multiplicity()->Draw();
-
-        jet_canvas->cd(2);
-        jets->leading_jet_pt()->Draw();
-
-        jet_canvas->cd(3);
-        jets->pt()->Draw();
-
-        shared_ptr<TCanvas> gen_particle_canvas(new TCanvas("gen_particles", "GenParticles", 800, 320));
-        gen_particle_canvas->Divide(3);
-
-        gen_particle_canvas->cd(1);
-        gen_particles->id()->Draw();
-
-        gen_particle_canvas->cd(2);
-        gen_particles->status()->Draw();
-
-        gen_particle_canvas->cd(3);
-        gen_particles->pt()->Draw();
-
-        shared_ptr<TCanvas> muon_canvas(new TCanvas("muons", "Muons", 800, 320));
-        muon_canvas->Divide(3);
-
-        muon_canvas->cd(1);
-        muons->multiplicity()->Draw();
-
-        muon_canvas->cd(2);
-        muons->leading_muon_pt()->Draw();
-
-        muon_canvas->cd(3);
-        muons->pt()->Draw();
-
-        shared_ptr<TCanvas> electron_canvas(new TCanvas("electrons", "Electrons", 800, 320));
-        electron_canvas->Divide(3);
-
-        electron_canvas->cd(1);
-        electrons->multiplicity()->Draw();
-
-        electron_canvas->cd(2);
-        electrons->leading_electron_pt()->Draw();
-
-        electron_canvas->cd(3);
-        electrons->pt()->Draw();
-
-        shared_ptr<TCanvas> primary_vertex_canvas(new TCanvas("primary_vertices", "Priamary Vertices", 640, 480));
-        primary_vertex_canvas->Divide(2, 2);
-
-        primary_vertex_canvas->cd(1);
-        primary_vertices->multiplicity()->Draw();
-
-        primary_vertex_canvas->cd(2);
-        primary_vertices->x()->Draw();
-
-        primary_vertex_canvas->cd(3);
-        primary_vertices->y()->Draw();
-
-        primary_vertex_canvas->cd(4);
-        primary_vertices->z()->Draw();
-
-        shared_ptr<TCanvas> missing_energy_canvas(new TCanvas("missing_energy", "Missing Energy", 640, 480));
-        missing_energy_canvas->Divide(2, 2);
-
-        missing_energy_canvas->cd(1);
-        missing_energy->pt()->Draw();
-
-        missing_energy_canvas->cd(2);
-        missing_energy->x()->Draw();
-
-        missing_energy_canvas->cd(3);
-        missing_energy->y()->Draw();
-
-        missing_energy_canvas->cd(4);
-        missing_energy->z()->Draw();
-
-        app->Run();
-        */
+        result = 1;
     }
-
-    cout << *jets << endl;
-    cout << *gen_particles << endl;
-    cout << *muons << endl;
-    cout << *electrons << endl;
-    cout << *primary_vertices << endl;
-    cout << *missing_energy << endl;
-
-    jets.reset();
 
     // Clean Up any memory allocated by libprotobuf
     //
     google::protobuf::ShutdownProtobufLibrary();
 
-    return 0;
+    return result;
+}
+
+void run(const Files &input_files)
+try
+{
+    // Prepare Analysis
+    //
+    shared_ptr<ThreadController> controller(new ThreadController());
+    MonitorAnalyzerPtr analyzer(new MonitorAnalyzer());
+
+    // Process inputs
+    //
+    controller->process(analyzer, input_files);
+
+    // Plot results
+    //
+    plot(analyzer);
+
 }
 catch(...)
 {
-    // Clean Up any memory allocated by libprotobuf
+}
+
+void plot(const MonitorAnalyzerPtr &analyzer)
+{
+    // Cheat ROOT with empty args
     //
-    google::protobuf::ShutdownProtobufLibrary();
+    int empty_argc = 1;
+    char *empty_argv[] = { "root" };
+    shared_ptr<TRint> app(new TRint("app", &empty_argc, empty_argv));
 
-    cerr << "Unknown error" << endl;
+    shared_ptr<TCanvas> jet_canvas(new TCanvas("jets", "Jets", 800, 320));
+    jet_canvas->Divide(3);
 
-    return 1;
+    jet_canvas->cd(1);
+    TH1Ptr jet_multiplicity = convert(*analyzer->monitorJets()->multiplicity());
+    jet_multiplicity->Draw();
+
+    jet_canvas->cd(2);
+    TH1Ptr jet_leading_pt = convert(*analyzer->monitorJets()->leading_pt());
+    jet_leading_pt->Draw();
+
+    jet_canvas->cd(3);
+    TH1Ptr jet_pt = convert(*analyzer->monitorJets()->pt());
+    jet_pt->Draw();
+
+    /*
+    shared_ptr<TCanvas> gen_particle_canvas(new TCanvas("gen_particles", "GenParticles", 800, 320));
+    gen_particle_canvas->Divide(3);
+
+    gen_particle_canvas->cd(1);
+    gen_particles->id()->Draw();
+
+    gen_particle_canvas->cd(2);
+    gen_particles->status()->Draw();
+
+    gen_particle_canvas->cd(3);
+    gen_particles->pt()->Draw();
+
+    shared_ptr<TCanvas> muon_canvas(new TCanvas("muons", "Muons", 800, 320));
+    muon_canvas->Divide(3);
+
+    muon_canvas->cd(1);
+    muons->multiplicity()->Draw();
+
+    muon_canvas->cd(2);
+    muons->leading_muon_pt()->Draw();
+
+    muon_canvas->cd(3);
+    muons->pt()->Draw();
+
+    shared_ptr<TCanvas> electron_canvas(new TCanvas("electrons", "Electrons", 800, 320));
+    electron_canvas->Divide(3);
+
+    electron_canvas->cd(1);
+    electrons->multiplicity()->Draw();
+
+    electron_canvas->cd(2);
+    electrons->leading_electron_pt()->Draw();
+
+    electron_canvas->cd(3);
+    electrons->pt()->Draw();
+
+    shared_ptr<TCanvas> primary_vertex_canvas(new TCanvas("primary_vertices", "Priamary Vertices", 640, 480));
+    primary_vertex_canvas->Divide(2, 2);
+
+    primary_vertex_canvas->cd(1);
+    primary_vertices->multiplicity()->Draw();
+
+    primary_vertex_canvas->cd(2);
+    primary_vertices->x()->Draw();
+
+    primary_vertex_canvas->cd(3);
+    primary_vertices->y()->Draw();
+
+    primary_vertex_canvas->cd(4);
+    primary_vertices->z()->Draw();
+
+    shared_ptr<TCanvas> missing_energy_canvas(new TCanvas("missing_energy", "Missing Energy", 640, 480));
+    missing_energy_canvas->Divide(2, 2);
+
+    missing_energy_canvas->cd(1);
+    missing_energy->pt()->Draw();
+
+    missing_energy_canvas->cd(2);
+    missing_energy->x()->Draw();
+
+    missing_energy_canvas->cd(3);
+    missing_energy->y()->Draw();
+
+    missing_energy_canvas->cd(4);
+    missing_energy->z()->Draw();
+    */
+
+    app->Run();
 }
