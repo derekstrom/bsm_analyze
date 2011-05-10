b1e467f158f545d8fd6db2d0059f2e8456121d74
Samvel Khalatyan <ksamdev@gmail.com>
2011-05-10 10:52:03 -0500

    Forward bsm classes declarations

---
+++ interface/bsmfwd.h
@@ -0,0 +1,19 @@
+// Forward declarations
+//
+// Analysis classes forward declarations
+//
+// Created by Samvel Khalatyan, May 10, 2011
+// Copyright 2011, All rights reserved
+
+#ifndef BSM_FORWARD
+#define BSM_FORWARD
+
+namespace bsm
+{
+    namespace algorithm
+    {
+        class ClosestJet;
+    }
+}
+
+#endif
