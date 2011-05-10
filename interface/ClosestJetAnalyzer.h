e1b0f8e9197136e1a8e447e13ac6f29b058c9132
Samvel Khalatyan <ksamdev@gmail.com>
2011-05-10 15:29:16 -0500

    Add electron/muon delta R vs pTrel 2D histograms

--- interface/ClosestJetAnalyzer.h
+++ interface/ClosestJetAnalyzer.h
@@ -10,8 +10,11 @@
 
 #include <boost/shared_ptr.hpp>
 
+#include "bsm_stat/interface/statfwd.h"
 #include "interface/Analyzer.h"
 #include "interface/bsmfwd.h"
+
+class TLorentzVector;
 
 namespace bsm
 {
@@ -26,6 +29,7 @@ namespace bsm
             typedef boost::shared_ptr<DeltaMonitor> DeltaMonitorPtr;
             typedef boost::shared_ptr<LorentzVectorMonitor> 
                 LorentzVectorMonitorPtr;
+            typedef boost::shared_ptr<stat::H2> H2Ptr;
 
             ClosestJetAnalyzer();
             virtual ~ClosestJetAnalyzer();
@@ -47,10 +51,12 @@ namespace bsm
             const LorentzVectorMonitorPtr monitorElectrons() const;
             const LorentzVectorMonitorPtr monitorElectronJets() const;
             const DeltaMonitorPtr monitorElectronDelta() const;
+            const H2Ptr electronDelta() const;
 
             const LorentzVectorMonitorPtr monitorMuons() const;
             const LorentzVectorMonitorPtr monitorMuonJets() const;
             const DeltaMonitorPtr monitorMuonDelta() const;
+            const H2Ptr muonDelta() const;
 
         private:
             void processElectrons(const Event *event);
@@ -63,12 +69,21 @@ namespace bsm
             LorentzVectorMonitorPtr _monitor_electrons;
             LorentzVectorMonitorPtr _monitor_electron_jets;
             DeltaMonitorPtr _monitor_electron_delta;
+            H2Ptr _electron_delta;
 
             LorentzVectorMonitorPtr _monitor_muons;
             LorentzVectorMonitorPtr _monitor_muon_jets;
             DeltaMonitorPtr _monitor_muon_delta;
+            H2Ptr _muon_delta;
 
             boost::shared_ptr<algorithm::ClosestJet> _closest_jet_finder;
+
+            typedef boost::shared_ptr<TLorentzVector> P4;
+
+            // Temporary Lorentz Vectors that are used to calculate pTrel
+            //
+            P4 _p4_1;
+            P4 _p4_2;
     };
 }
 
--- src/ClosestJetAnalyzer.cc
+++ src/ClosestJetAnalyzer.cc
@@ -11,13 +11,17 @@
 
 #include <boost/pointer_cast.hpp>
 
+#include "TLorentzVector.h"
+
 #include "bsm_input/interface/Event.pb.h"
 #include "bsm_input/interface/Electron.pb.h"
 #include "bsm_input/interface/Muon.pb.h"
 #include "bsm_input/interface/Physics.pb.h"
+#include "bsm_stat/interface/H2.h"
 #include "interface/Algorithm.h"
 #include "interface/Monitor.h"
 #include "interface/ClosestJetAnalyzer.h"
+#include "interface/Utility.h"
 
 using std::cerr;
 using std::endl;
@@ -25,6 +29,8 @@ using std::endl;
 using boost::dynamic_pointer_cast;
 
 using bsm::ClosestJetAnalyzer;
+using bsm::stat::H1;
+using bsm::stat::H2;
 
 ClosestJetAnalyzer::ClosestJetAnalyzer()
 try
@@ -32,12 +38,17 @@ try
     _monitor_electrons.reset(new LorentzVectorMonitor());
     _monitor_electron_jets.reset(new LorentzVectorMonitor());
     _monitor_electron_delta.reset(new DeltaMonitor());
+    _electron_delta.reset(new H2(50, 0, 10, 50, 0, 5));
 
     _monitor_muons.reset(new LorentzVectorMonitor());
     _monitor_muon_jets.reset(new LorentzVectorMonitor());
     _monitor_muon_delta.reset(new DeltaMonitor());
+    _muon_delta.reset(new H2(200, 0, 200, 50, 0, 5));
 
     _closest_jet_finder.reset(new algorithm::ClosestJet());
+
+    _p4_1.reset(new TLorentzVector());
+    _p4_2.reset(new TLorentzVector());
 }
 catch(const std::bad_alloc &)
 {
@@ -47,10 +58,12 @@ catch(const std::bad_alloc &)
     _monitor_electrons.reset();
     _monitor_electron_jets.reset();
     _monitor_electron_delta.reset();
+    _electron_delta.reset();
 
     _monitor_muons.reset();
     _monitor_muon_jets.reset();
     _monitor_muon_delta.reset();
+    _muon_delta.reset();
 
     _closest_jet_finder.reset();
 }
@@ -68,10 +81,12 @@ ClosestJetAnalyzer::AnalyzerPtr ClosestJetAnalyzer::clone() const
         *analyzer->_monitor_electrons = *_monitor_electrons;
         *analyzer->_monitor_electron_jets = *_monitor_electron_jets;
         *analyzer->_monitor_electron_delta = *_monitor_electron_delta;
+        *analyzer->_electron_delta = *_electron_delta;
 
         *analyzer->_monitor_muons = *_monitor_muons;
         *analyzer->_monitor_muon_jets = *_monitor_muon_jets;
         *analyzer->_monitor_muon_delta = *_monitor_muon_delta;
+        *analyzer->_muon_delta = *_muon_delta;
 
         *analyzer->_closest_jet_finder = *_closest_jet_finder;
     }
@@ -80,10 +95,12 @@ ClosestJetAnalyzer::AnalyzerPtr ClosestJetAnalyzer::clone() const
         analyzer->_monitor_electrons.reset();
         analyzer->_monitor_electron_jets.reset();
         analyzer->_monitor_electron_delta.reset();
+        analyzer->_electron_delta.reset();
 
         analyzer->_monitor_muons.reset();
         analyzer->_monitor_muon_jets.reset();
         analyzer->_monitor_muon_delta.reset();
+        analyzer->_muon_delta.reset();
 
         analyzer->_closest_jet_finder.reset();
     }
@@ -104,10 +121,12 @@ void ClosestJetAnalyzer::merge(const AnalyzerPtr &analyzer)
     bsm::merge(*_monitor_electrons, *analyzer_ptr->_monitor_electrons);
     bsm::merge(*_monitor_electron_jets, *analyzer_ptr->_monitor_electron_jets);
     bsm::merge(*_monitor_electron_delta, *analyzer_ptr->_monitor_electron_delta);
+    *_electron_delta += *analyzer_ptr->_electron_delta;
     
     bsm::merge(*_monitor_muons, *analyzer_ptr->_monitor_muons);
     bsm::merge(*_monitor_muon_jets, *analyzer_ptr->_monitor_muon_jets);
     bsm::merge(*_monitor_muon_delta, *analyzer_ptr->_monitor_muon_delta);
+    *_muon_delta += *analyzer_ptr->_muon_delta;
 }
 
 void ClosestJetAnalyzer::process(const Event *event)
@@ -134,10 +153,12 @@ void ClosestJetAnalyzer::print(std::ostream &out) const
     out << *_monitor_electrons << endl;
     out << *_monitor_electron_jets << endl;
     out << *_monitor_electron_delta << endl;
+    out << *_electron_delta << endl;
 
     out << *_monitor_muons << endl;
     out << *_monitor_muon_jets << endl;
     out << *_monitor_muon_delta << endl;
+    out << *_muon_delta << endl;
 }
 
 ClosestJetAnalyzer::operator bool() const
@@ -145,10 +166,12 @@ ClosestJetAnalyzer::operator bool() const
     return _monitor_electrons
         && _monitor_electron_jets
         && _monitor_electron_delta
+        && _electron_delta
 
         && _monitor_muons
         && _monitor_muon_jets
         && _monitor_muon_delta
+        && _muon_delta
 
         && _closest_jet_finder;
 }
@@ -171,6 +194,11 @@ const ClosestJetAnalyzer::DeltaMonitorPtr
     return _monitor_electron_delta;
 }
 
+const ClosestJetAnalyzer::H2Ptr ClosestJetAnalyzer::electronDelta() const
+{
+    return _electron_delta;
+}
+
 const ClosestJetAnalyzer::LorentzVectorMonitorPtr
     ClosestJetAnalyzer::monitorMuons() const
 {
@@ -187,6 +215,11 @@ const ClosestJetAnalyzer::DeltaMonitorPtr
     ClosestJetAnalyzer::monitorMuonDelta() const
 {
     return _monitor_muon_delta;
+}
+
+const ClosestJetAnalyzer::H2Ptr ClosestJetAnalyzer::muonDelta() const
+{
+    return _muon_delta;
 }
 
 // Privates
@@ -209,6 +242,12 @@ void ClosestJetAnalyzer::processElectrons(const Event *event)
         _monitor_electron_jets->fill(jet->physics_object().p4());
         _monitor_electron_delta->fill(jet->physics_object().p4(),
                 electron->physics_object().p4());
+
+        utility::set(_p4_1.get(), &jet->physics_object().p4());
+        utility::set(_p4_2.get(), &electron->physics_object().p4());
+
+        _electron_delta->fill(_p4_1->Vect().Perp(_p4_2->Vect()),
+                _p4_1->DeltaR(*_p4_2));
     }
 }
 
@@ -230,5 +269,11 @@ void ClosestJetAnalyzer::processMuons(const Event *event)
         _monitor_muon_jets->fill(jet->physics_object().p4());
         _monitor_muon_delta->fill(jet->physics_object().p4(),
                 muon->physics_object().p4());
+
+        utility::set(_p4_1.get(), &jet->physics_object().p4());
+        utility::set(_p4_2.get(), &muon->physics_object().p4());
+
+        _muon_delta->fill(_p4_1->Vect().Perp(_p4_2->Vect()),
+                _p4_1->DeltaR(*_p4_2));
     }
 }
