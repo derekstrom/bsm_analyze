b2621c1a9a29465ec27a19d3a646d3d150ef9dde
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2011-07-17 19:40:44 -0500

    Finish the Synch Exercise July 2011 v2 with JEC

--- interface/SynchAnalyzer.h
+++ interface/SynchAnalyzer.h
@@ -10,12 +10,16 @@
 #define BSM_SYNCHRONIZATION_ANALYZER
 
 #include <vector>
+#include <sstream>
 
 #include <boost/shared_ptr.hpp>
 
 #include "bsm_input/interface/Event.pb.h"
+#include "JetMETObjects/interface/JetCorrectorParameters.h"
 #include "interface/Analyzer.h"
 #include "interface/bsm_fwd.h"
+
+class FactorizedJetCorrector;
 
 namespace bsm
 {
@@ -97,7 +101,115 @@ namespace bsm
             P4MonitorPtr _muon_after_veto;
     };
 
+    class SynchJECJuly2011Analyzer : public Analyzer
+    {
+        public:
+            typedef boost::shared_ptr<LorentzVectorMonitor> P4MonitorPtr;
+            typedef std::vector<JetCorrectorParameters> Corrections;
+
+            enum LeptonMode
+            {
+                ELECTRON = 1,
+                MUON = 2
+            };
+
+            SynchJECJuly2011Analyzer(const LeptonMode & = ELECTRON);
+            SynchJECJuly2011Analyzer(const SynchJECJuly2011Analyzer &);
+
+            void setJetEnergyCorrections(const Corrections &corrections);
+
+            const P4MonitorPtr leadingJet() const;
+
+            const P4MonitorPtr electronBeforeVeto() const;
+            const P4MonitorPtr muonToVeto() const;
+            const P4MonitorPtr electronAfterVeto() const;
+
+            const P4MonitorPtr muonBeforeVeto() const;
+            const P4MonitorPtr electronToVeto() const;
+            const P4MonitorPtr muonAfterVeto() const;
+
+            // Anlayzer interface
+            //
+            virtual void onFileOpen(const std::string &filename, const Input *);
+            virtual void process(const Event *);
+
+            // Object interface
+            //
+            virtual uint32_t id() const;
+
+            virtual ObjectPtr clone() const;
+            virtual void merge(const ObjectPtr &);
+
+            virtual void print(std::ostream &) const;
+
+        private:
+            typedef boost::shared_ptr<MultiplicityCutflow> CutflowPtr;
+            typedef std::vector<const Electron *> GoodElectrons;
+
+            struct CorrectedJet
+            {
+                CorrectedJet()
+                {
+                    jet = 0;
+                }
+
+                const Jet *jet;
+                LorentzVector corrected_p4;
+            };
+
+            typedef std::vector<CorrectedJet> GoodJets;
+            typedef std::vector<const Muon *> GoodMuons;
+
+            enum
+            {
+                PRESELECTION = 0,
+                SCRAPING,
+                HBHENOISE,
+                PRIMARY_VERTEX,
+                JET,
+                LEPTON,
+                VETO_SECOND_LEPTON
+            };
+
+            void copyCorrections(const Corrections &);
+
+            GoodJets jets(const Event *,
+                    const GoodElectrons &,
+                    const GoodMuons &);
+
+            void printP4(std::ostream &, const LorentzVector &);
+
+            LeptonMode _lepton_mode;
+
+            CutflowPtr _cutflow;
+
+            boost::shared_ptr<PrimaryVertexSelector> _primary_vertex_selector;
+            boost::shared_ptr<JetSelector> _jet_selector;
+            boost::shared_ptr<ElectronSelector> _electron_selector;
+            boost::shared_ptr<ElectronSelector> _electron_veto_selector;
+            boost::shared_ptr<MuonSelector> _muon_selector;
+            boost::shared_ptr<MuonSelector> _muon_veto_selector;
+
+            std::vector<Event::Extra> _passed_events;
+
+            P4MonitorPtr _leading_jet;
+
+            P4MonitorPtr _electron_before_veto;
+            P4MonitorPtr _muon_to_veto;
+            P4MonitorPtr _electron_after_veto;
+
+            P4MonitorPtr _muon_before_veto;
+            P4MonitorPtr _electron_to_veto;
+            P4MonitorPtr _muon_after_veto;
+
+            Corrections _corrections;
+            boost::shared_ptr<FactorizedJetCorrector> _jec;
+
+            std::ostringstream _out;
+    };
+
     std::ostream &operator <<(std::ostream &, const SynchJuly2011Analyzer::LeptonMode &);
+    std::ostream &operator <<(std::ostream &, const SynchJECJuly2011Analyzer::LeptonMode &);
 }
 
 #endif
--- src/SynchAnalyzer.cc
+++ src/SynchAnalyzer.cc
@@ -8,12 +8,14 @@
 
 #include <ostream>
 #include <string>
+#include <utility>
 
 #include <boost/pointer_cast.hpp>
 
 #include "bsm_core/interface/ID.h"
 #include "bsm_input/interface/Algebra.h"
 #include "bsm_input/interface/Event.pb.h"
+#include "JetMETObjects/interface/FactorizedJetCorrector.h"
 #include "interface/Monitor.h"
 #include "interface/Selector.h"
 #include "interface/SynchAnalyzer.h"
@@ -23,17 +25,18 @@ using namespace std;
 using boost::dynamic_pointer_cast;
 
 using bsm::SynchJuly2011Analyzer;
+using bsm::SynchJECJuly2011Analyzer;
 
 SynchJuly2011Analyzer::SynchJuly2011Analyzer(const LeptonMode &mode):
     _lepton_mode(mode)
 {
     _cutflow.reset(new MultiplicityCutflow(4));
-    _cutflow->cut(0)->setName("pre-selection");
-    _cutflow->cut(1)->setName("Good Primary Vertex");
-    _cutflow->cut(2)->setName("2 Good Jets");
-    _cutflow->cut(3)->setName(string("Good ")
+    _cutflow->cut(PRESELECTION)->setName("pre-selection");
+    _cutflow->cut(PRIMARY_VERTEX)->setName("Good Primary Vertex");
+    _cutflow->cut(JET)->setName("2 Good Jets");
+    _cutflow->cut(LEPTON)->setName(string("Good ")
             + (ELECTRON == _lepton_mode ? "Electron" : "Muon"));
-    _cutflow->cut(4)->setName(string("Veto Good ")
+    _cutflow->cut(VETO_SECOND_LEPTON)->setName(string("Veto Good ")
             + (ELECTRON == _lepton_mode ? "Muon" : "Electron"));
     monitor(_cutflow);
 
@@ -331,7 +334,7 @@ bool SynchJuly2011Analyzer::jets(const Event *event)
 
     uint32_t selected_jets = 0;
     const Jet *leading_jet = 0;
-    double leading_jet_pt = 0;
+    float leading_jet_pt = 0;
     LockSelectorEventCounterOnUpdate lock(*_jet_selector);
     for(Jets::const_iterator jet = event->jets().begin();
             event->jets().end() != jet
@@ -342,7 +345,7 @@ bool SynchJuly2011Analyzer::jets(const Event *event)
         {
             ++selected_jets;
 
-            const double jet_pt = pt(jet->physics_object().p4());
+            const float jet_pt = pt(jet->physics_object().p4());
             if (!leading_jet
                     || jet_pt > leading_jet_pt)
             {
@@ -481,6 +484,545 @@ bool SynchJuly2011Analyzer::muon(const Event *event)
 
 
 
+// Synch with Jet Energy corrections
+//
+SynchJECJuly2011Analyzer::SynchJECJuly2011Analyzer(const LeptonMode &mode):
+    _lepton_mode(mode)
+{
+    _cutflow.reset(new MultiplicityCutflow(6));
+    _cutflow->cut(PRESELECTION)->setName("pre-selection");
+    _cutflow->cut(SCRAPING)->setName("Scraping Veto");
+    _cutflow->cut(HBHENOISE)->setName("HBHE Noise");
+    _cutflow->cut(PRIMARY_VERTEX)->setName("Good Primary Vertex");
+    _cutflow->cut(JET)->setName("2 Good Jets");
+    _cutflow->cut(LEPTON)->setName(string("Good ")
+            + (ELECTRON == _lepton_mode ? "Electron" : "Muon"));
+    _cutflow->cut(VETO_SECOND_LEPTON)->setName(string("Veto Good ")
+            + (ELECTRON == _lepton_mode ? "Muon" : "Electron"));
+    monitor(_cutflow);
+
+    // Selectrors
+    //
+    _primary_vertex_selector.reset(new PrimaryVertexSelector());
+    _jet_selector.reset(new JetSelector());
+    _electron_selector.reset(new ElectronSelector());
+    _electron_veto_selector.reset(new ElectronSelector());
+    _muon_selector.reset(new MuonSelector());
+    _muon_veto_selector.reset(new MuonSelector());
+
+    _electron_selector->primary_vertex()->disable();
+    _electron_veto_selector->primary_vertex()->disable();
+
+    _muon_selector->primary_vertex()->disable();
+    _muon_veto_selector->primary_vertex()->disable();
+
+    _muon_selector->pt()->setValue(35);
+    _muon_veto_selector->pt()->setValue(35);
+
+    monitor(_primary_vertex_selector);
+    monitor(_jet_selector);
+    monitor(_electron_selector);
+    monitor(_electron_veto_selector);
+    monitor(_muon_selector);
+    monitor(_muon_veto_selector);
+
+    // Monitors
+    //
+    _leading_jet.reset(new LorentzVectorMonitor());
+
+    _electron_before_veto.reset(new LorentzVectorMonitor());
+    _muon_to_veto.reset(new LorentzVectorMonitor());
+    _electron_after_veto.reset(new LorentzVectorMonitor());
+
+    _muon_before_veto.reset(new LorentzVectorMonitor());
+    _electron_to_veto.reset(new LorentzVectorMonitor());
+    _muon_after_veto.reset(new LorentzVectorMonitor());
+
+    monitor(_leading_jet);
+
+    monitor(_electron_before_veto);
+    monitor(_muon_to_veto);
+    monitor(_electron_after_veto);
+
+    monitor(_muon_before_veto);
+    monitor(_electron_to_veto);
+    monitor(_muon_after_veto);
+}
+
+SynchJECJuly2011Analyzer::SynchJECJuly2011Analyzer(const SynchJECJuly2011Analyzer &object):
+    _lepton_mode(object._lepton_mode)
+{
+    setJetEnergyCorrections(object._corrections);
+
+    _cutflow = 
+        dynamic_pointer_cast<MultiplicityCutflow>(object._cutflow->clone());
+
+    monitor(_cutflow);
+
+    // Selectors
+    //
+    _primary_vertex_selector = 
+        dynamic_pointer_cast<PrimaryVertexSelector>(object._primary_vertex_selector->clone());
+
+    _jet_selector = 
+        dynamic_pointer_cast<JetSelector>(object._jet_selector->clone());
+
+    _electron_selector = 
+        dynamic_pointer_cast<ElectronSelector>(object._electron_selector->clone());
+
+    _electron_veto_selector = 
+        dynamic_pointer_cast<ElectronSelector>(object._electron_veto_selector->clone());
+
+    _muon_selector = 
+        dynamic_pointer_cast<MuonSelector>(object._muon_selector->clone());
+
+    _muon_veto_selector = 
+        dynamic_pointer_cast<MuonSelector>(object._muon_veto_selector->clone());
+
+    monitor(_primary_vertex_selector);
+    monitor(_jet_selector);
+    monitor(_electron_selector);
+    monitor(_electron_veto_selector);
+    monitor(_muon_selector);
+    monitor(_muon_veto_selector);
+
+    // Monitors
+    //
+    // Monitors
+    //
+    _leading_jet = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._leading_jet->clone());
+
+    _electron_before_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._electron_before_veto->clone());
+
+    _muon_to_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._muon_to_veto->clone());
+
+    _electron_after_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._electron_after_veto->clone());
+
+    _muon_before_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._muon_before_veto->clone());
+
+    _electron_to_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._electron_to_veto->clone());
+
+    _muon_after_veto = 
+        dynamic_pointer_cast<LorentzVectorMonitor>(object._muon_after_veto->clone());
+
+    monitor(_leading_jet);
+
+    monitor(_electron_before_veto);
+    monitor(_muon_to_veto);
+    monitor(_electron_after_veto);
+
+    monitor(_muon_before_veto);
+    monitor(_electron_to_veto);
+    monitor(_muon_after_veto);
+}
+
+void SynchJECJuly2011Analyzer::setJetEnergyCorrections(const Corrections &corrections)
+{
+    copyCorrections(corrections);
+
+    _jec.reset(new FactorizedJetCorrector(_corrections));
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::leadingJet() const
+{
+    return _leading_jet;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::electronBeforeVeto() const
+{
+    return _electron_before_veto;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::muonToVeto() const
+{
+    return _muon_to_veto;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::electronAfterVeto() const
+{
+    return _electron_after_veto;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::muonBeforeVeto() const
+{
+    return _muon_before_veto;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::electronToVeto() const
+{
+    return _electron_to_veto;
+}
+
+const SynchJECJuly2011Analyzer::P4MonitorPtr SynchJECJuly2011Analyzer::muonAfterVeto() const
+{
+    return _muon_after_veto;
+}
+
+void SynchJECJuly2011Analyzer::onFileOpen(const std::string &filename, const Input *)
+{
+}
+
+void SynchJECJuly2011Analyzer::process(const Event *event)
+{
+    if (!_jec)
+        return;
+
+    _cutflow->apply(PRESELECTION);
+
+    if (event->has_filters()
+            && !event->filters().scraping_veto())
+        return;
+
+    _cutflow->apply(SCRAPING);
+
+    _cutflow->apply(HBHENOISE);
+
+    if (!event->primary_vertices().size()
+            || !_primary_vertex_selector->apply(*event->primary_vertices().begin()))
+        return;
+
+    _cutflow->apply(PRIMARY_VERTEX);
+
+    typedef ::google::protobuf::RepeatedPtrField<Electron> Electrons;
+
+    const PrimaryVertex &pv = *event->primary_vertices().begin();
+    GoodElectrons good_electrons;
+
+    LockSelectorEventCounterOnUpdate electron_lock(*_electron_selector);
+    for(Electrons::const_iterator electron = event->pf_electrons().begin();
+            event->pf_electrons().end() != electron;
+            ++electron)
+    {
+        if (_electron_selector->apply(*electron, pv))
+            good_electrons.push_back(&*electron);
+    }
+
+    typedef ::google::protobuf::RepeatedPtrField<Muon> Muons;
+
+    GoodMuons good_muons;
+
+    LockSelectorEventCounterOnUpdate muon_lock(*_muon_selector);
+    for(Muons::const_iterator muon = event->pf_muons().begin();
+            event->pf_muons().end() != muon;
+            ++muon)
+    {
+        if (_muon_selector->apply(*muon, pv))
+            good_muons.push_back(&*muon);
+    }
+
+    GoodJets good_jets = jets(event, good_electrons, good_muons);
+
+    if (2 > good_jets.size())
+        return;
+
+    _cutflow->apply(JET);
+
+    if (ELECTRON == _lepton_mode)
+    {
+        if (1 != good_electrons.size())
+            return;
+
+        _cutflow->apply(LEPTON);
+
+        if (good_muons.size())
+            return;
+    }
+    else if (MUON == _lepton_mode)
+    {
+        if (1 != good_muons.size())
+            return;
+
+        _cutflow->apply(LEPTON);
+
+        if (good_electrons.size())
+            return;
+    }
+    else
+        return;
+
+    _cutflow->apply(VETO_SECOND_LEPTON);
+
+    _passed_events.push_back(event->extra());
+}
+
+uint32_t SynchJECJuly2011Analyzer::id() const
+{
+    return core::ID<SynchJECJuly2011Analyzer>::get();
+}
+
+SynchJECJuly2011Analyzer::ObjectPtr SynchJECJuly2011Analyzer::clone() const
+{
+    return ObjectPtr(new SynchJECJuly2011Analyzer(*this));
+}
+
+void SynchJECJuly2011Analyzer::merge(const ObjectPtr &pointer)
+{
+    if (pointer->id() != id())
+        return;
+
+    boost::shared_ptr<SynchJECJuly2011Analyzer> object =
+        dynamic_pointer_cast<SynchJECJuly2011Analyzer>(pointer);
+
+    if (!object)
+        return;
+
+    Object::merge(pointer);
+
+    _passed_events.insert(_passed_events.end(),
+            object->_passed_events.begin(),
+            object->_passed_events.end());
+
+    _out << endl;
+    _out << object->_out.str();
+}
+
+void SynchJECJuly2011Analyzer::print(std::ostream &out) const
+{
+    out << "Survived Events" << endl;
+    out << " " << setw(10) << left << "Run" << setw(10) << "Lumi" << "Event" << endl;
+    for(std::vector<Event::Extra>::const_iterator extra = _passed_events.begin();
+            _passed_events.end() != extra;
+            ++extra)
+    {
+        out << " "
+            << setw(10) << left << extra->run()
+            << setw(10) << left << extra->lumi()
+            << extra->id() << endl;
+    }
+    out << endl;
+
+    out << "Cutflow [" << _lepton_mode << " mode]" << endl;
+    out << *_cutflow << endl;
+    out << endl;
+
+    out << "Jet Selector" << endl;
+    out << *_jet_selector << endl;
+    out << endl;
+
+    switch(_lepton_mode)
+    {
+        case ELECTRON: out << "Electron Selector" << endl;
+                       out << *_electron_selector << endl;
+                       out << endl;
+                       out << "Muon Veto" << endl;
+                       out << *_muon_veto_selector << endl;
+                       out << endl;
+                       out << "Electron before Veto" << endl;
+                       out << *electronBeforeVeto() << endl;
+                       out << endl;
+                       out << "Muon to Veto" << endl;
+                       out << *muonToVeto() << endl;
+                       out << endl;
+                       out << "Electron after Veto" << endl;
+                       out << *electronAfterVeto();
+                       break;
+
+        case MUON:     out << "Muon Selector" << endl;
+                       out << *_muon_selector << endl;
+                       out << endl;
+                       out << "Electron Veto" << endl;
+                       out << *_electron_veto_selector << endl;
+                       out << endl;
+                       out << "Muon before Veto" << endl;
+                       out << *muonBeforeVeto() << endl;
+                       out << endl;
+                       out << "Electron to Veto" << endl;
+                       out << *electronToVeto() << endl;
+                       out << endl;
+                       out << "Muon after Veto" << endl;
+                       out << *muonAfterVeto();
+                       break;
+
+        default: break;
+    }
+
+    out << endl;
+    out << _out.str();
+}
+
+// Private
+//
+void SynchJECJuly2011Analyzer::copyCorrections(const Corrections &corrections)
+{
+    _corrections.clear();
+
+    if (!corrections.size())
+        return;
+
+    _corrections.resize(corrections.size());
+    copy(corrections.begin(), corrections.end(), _corrections.begin());
+}
+
+SynchJECJuly2011Analyzer::GoodJets
+    SynchJECJuly2011Analyzer::jets(const Event *event,
+            const GoodElectrons &electrons,
+            const GoodMuons &muons)
+{
+    GoodJets good_jets;
+
+    typedef ::google::protobuf::RepeatedPtrField<Jet> Jets;
+    typedef ::google::protobuf::RepeatedPtrField<Jet::Child> Children;
+
+    const float rho = event->extra().rho();
+
+    std::ostringstream jets_out;
+
+    std::ostringstream out;
+
+    /*
+    for(GoodElectrons::const_iterator electron = electrons.begin();
+            electrons.end() != electron;
+            ++electron)
+    {
+        jets_out << "el: ";
+        printP4(jets_out, (*electron)->physics_object().p4());
+        jets_out << endl;
+    }
+
+    for(GoodMuons::const_iterator muon = muons.begin();
+            muons.end() != muon;
+            ++muon)
+    {
+        jets_out << "mu: ";
+        printP4(jets_out, (*muon)->physics_object().p4());
+        jets_out << endl;
+    }
+    */
+
+    LockSelectorEventCounterOnUpdate lock(*_jet_selector);
+    for(Jets::const_iterator jet = event->jets().begin();
+            event->jets().end() != jet;
+            ++jet)
+    {
+        // Skip jet is required information is not available
+        //
+        if (!jet->has_uncorrected_p4())
+            continue;
+
+        out.str("");
+
+        // Remove Leptons
+        //
+        LorentzVector corrected_p4 = jet->uncorrected_p4();
+        for(Children::const_iterator child = jet->children().begin();
+                jet->children().end() != child;
+                ++child)
+        {
+            const LorentzVector &child_p4 = child->physics_object().p4();
+            const float child_pt = pt(child_p4);
+            const float child_eta = eta(child_p4);
+            const float child_phi = phi(child_p4);
+
+            /*
+            out << "child: ";
+            printP4(out, child_p4);
+            out << endl;
+            out << child_pt << " " << child_eta << " " << child_phi << endl;
+            */
+
+            // Electrons
+            //
+            for(GoodElectrons::const_iterator electron = electrons.begin();
+                    electrons.end() != electron;
+                    ++electron)
+            {
+                const LorentzVector &electron_p4 = (*electron)->physics_object().p4();
+                if (child_pt == pt(electron_p4)
+                        && child_eta == eta(electron_p4)
+                        && child_phi == phi(electron_p4))
+                {
+                    corrected_p4 -= electron_p4;
+
+                    out << setw(20) << "Electron ";
+                    printP4(out, electron_p4);
+                    out << endl;
+                }
+            }
+
+            for(GoodMuons::const_iterator muon = muons.begin();
+                    muons.end() != muon;
+                    ++muon)
+            {
+                const LorentzVector &muon_p4 = (*muon)->physics_object().p4();
+                if (child_pt == pt(muon_p4)
+                        && child_eta == eta(muon_p4)
+                        && child_phi == phi(muon_p4))
+                {
+                    corrected_p4 -= muon_p4;
+
+                    out << setw(20) << "Muon ";
+                    printP4(out, muon_p4);
+                    out << endl;
+                }
+            }
+        }
+
+        // fix p4
+        //
+        _jec->setJetEta(eta(corrected_p4));
+        _jec->setJetPt(pt(corrected_p4));
+        _jec->setJetE(corrected_p4.e());
+        _jec->setNPV(event->primary_vertices().size());
+        _jec->setJetA(jet->extra().area());
+        _jec->setRho(rho);
+
+        float correction = _jec->getCorrection();
+        corrected_p4 *= correction;
+
+        Jet corrected_jet = *jet;
+
+        *corrected_jet.mutable_physics_object()->mutable_p4() = corrected_p4;
+
+        if (!out.str().empty())
+        {
+            jets_out << setw(20) << "Uncorrected Jet ";
+            printP4(jets_out,  jet->uncorrected_p4());
+            jets_out << endl << out.str();
+            jets_out << setw(20) << "Corrected Jet ";
+            printP4(jets_out, corrected_p4);
+            jets_out << endl;
+            jets_out << setw(20) << "PAT Jet ";
+            printP4(jets_out, jet->physics_object().p4());
+            jets_out << endl;
+        }
+
+        if (!_jet_selector->apply(corrected_jet))
+            continue;
+
+        CorrectedJet tmp;
+        tmp.jet = &*jet;
+        tmp.corrected_p4 = corrected_p4;
+
+        good_jets.push_back(tmp);
+    }
+
+    if (!jets_out.str().empty())
+    {
+        _out << "Event: " << event->extra().id()
+            << " Lumi: " << event->extra().lumi()
+            << " Run: " << event->extra().run() << endl;
+        _out << "good el: " << electrons.size() << " mu: " << muons.size() << endl;
+
+        _out << jets_out.str() << endl;
+    }
+
+    return good_jets;
+}
+
+void SynchJECJuly2011Analyzer::printP4(std::ostream &out, const LorentzVector &p4)
+{
+    out << "pT: " << pt(p4)
+    << " eta: " << eta(p4)
+    << " phi: " << phi(p4);
+}
+
+
 // Helpers
 //
 std::ostream &bsm::operator <<(std::ostream &out, const SynchJuly2011Analyzer::LeptonMode &mode)
@@ -491,6 +1033,23 @@ std::ostream &bsm::operator <<(std::ostream &out, const SynchJuly2011Analyzer::L
                                                 break;
 
         case SynchJuly2011Analyzer::MUON:       out << "muon";
+                                                break;
+
+        default:                                out << "unknown";
+                                                break;
+    }
+
+    return out;
+}
+
+std::ostream &bsm::operator <<(std::ostream &out, const SynchJECJuly2011Analyzer::LeptonMode &mode)
+{
+    switch(mode)
+    {
+        case SynchJECJuly2011Analyzer::ELECTRON:   out << "electron";
+                                                break;
+
+        case SynchJECJuly2011Analyzer::MUON:       out << "muon";
                                                 break;
 
         default:                                out << "unknown";
